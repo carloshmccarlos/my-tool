818ac1a51a5030d4974172f51a417f14dd09df16
carloshmccarlos <carloshmccarlos@gmail.com>
2024-09-29 11:43:05 +0800

    1

--- app/json-comparison/page.tsx
+++ app/json-comparison/page.tsx
@@ -1,4 +1,10 @@
 import {JsonComparison} from "@/components/json-comparison";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Json Comparison",
+    description: "A tool that can compare two JSON strings to find the differences.",
+};
 
 export default function Page(){
     return (
